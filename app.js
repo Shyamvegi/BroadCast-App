d649800c8f28026da2164e941e775564ce044844
Shyam Vegi <54716212+Shyamvegi@users.noreply.github.com>
2021-09-08 23:37:09 +0530

    Added disconnected message

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-
+//User disconnected
 
   socket.on('chat message', function(msg){
     console.log(msg);
